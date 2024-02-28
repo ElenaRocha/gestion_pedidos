2805b7025f3b6161ff918a7994676bf3fe5bd73b
Elena Rocha <ele_r.v@hotmail.com>
2024-02-28 22:38:33 +0100

    formulario login

--- login.php
+++ login.php
@@ -17,3 +17,26 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     header("Location: categorias.php");
     return;
 }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login Tiendas</title>
+        <meta charset = "UTF-8">
+    </head>
+    <body>
+        <?php if(isset($_GET["redirigido"])) {
+            echo "<p>Introduzca sus credenciales: </p>";
+        }?>
+        <?php if(isset($err) and $err == TRUE) {
+            echo "<p>Credenciales incorrectas</p>";
+        }
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method = "POST">
+            <label for = "usuario">email</label>
+            <input value = "<?php if(isset($usuario)) echo $usuario;?>" id = "usuario" name = "usuario" type="text">
+            <label for="clave">Contraseña</label>
+            <input id = "clave" name = "cave" type="password">
+            <input type="submit">
+        </form>
+    </body>
+</html>    

e7d3b8197d30e5099f4c329d95eddbc450f34e30
Elena Rocha <ele_r.v@hotmail.com>
2024-02-28 22:43:14 +0100

    cabecera

---
+++ cabecera.php
@@ -0,0 +1,7 @@
+<header>
+    Tienda: <?php echo $_SESSION['tienda_id']." ".$_SESSION['email'];?>
+    <a href="categorias.php">Inicio</a>
+    <a href="carrito.php">Carrito</a>
+    <a href="logout.php">Cerrar sesión</a>
+</header>
+<hr>

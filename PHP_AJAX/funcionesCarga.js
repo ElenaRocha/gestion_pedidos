1ca437a6be04d2cf2095a2d9d1bed680c1e06843
Elena Rocha <ele_r.v@hotmail.com>
2024-03-21 21:05:09 +0100

    completar fichero ajax

--- PHP_AJAX/funcionesCarga.js
+++ PHP_AJAX/funcionesCarga.js
@@ -34,3 +34,169 @@ function crearVinculoCategorias(nombre, codCategoria) {
     vinculo.onclick = function(){return cargarProductos(this);}
     return vinculo;
 }
+
+function cargarProductos(destino) {
+    var XMLHttpRequestObject = new XMLHttpRequest();
+
+    XMLHttpRequestObject.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var titulo = document.getElementById("titulo");
+            var contenido = document.getElementById("contenido");
+            titulo.innerHTML = "Productos";
+
+            try {
+                var productos_json = JSON.parse(this.responseText);
+                var tabla = crearTablaProductos(productos_json);
+                contenido.innerHTML = "";
+                contenido.appendChild(tabla);
+            } catch(e) {
+                var mensaje = document.createElement("p");
+                mensaje.innerHTML = "Categoria sin productos";
+                contenido.innerHTML = "";
+                contenido.appendChild(mensaje);
+            }
+        }
+    };
+
+    XMLHttpRequestObject.open("GET", destino, true);
+    XMLHttpRequestObject.send();
+    return false;
+}
+
+function crearTablaProductos(productos) {
+    var tabla = document.createElement("tabla");
+    var cabecera = crearFila(["Código", "Nombre", "Descripción", "Stock", "Comprar"], "th");
+    tabla.appendChild(cabecera);
+    for(var i = 0; i<productos.length; i++) {
+        formulario = crearFormulario("Añadir", productos[i]["producto_id"], anadirProductos);
+        fila = crearFila([productos[i]["producto_id"],
+                                productos[i]["nombre"],
+                                productos[i]["descripción"],
+                                productos[i]["stock"]], "td");
+        celda = document.createElement("td");
+        celda.appendChild(formulario);
+        fila.appendChild(celda);
+        tabla.appendChild(fila);
+    }
+    return tabla;
+}
+
+function crearFormulario(texto, cod, funcion) {
+    var formulario = document.createElement("form");
+    var unidades = document.createElement("input");
+    unidades.value = 1;
+    unidades.name = "unidades";
+    var codigo = document.createElement("input");
+    codigo.value = cod;
+    codigo.type = "hidden";
+    codigo.name = "cod";
+    var boton = document.createElement("input");
+    boton.type = "submit";
+    boton.value = texto;
+    formulario.onsubmit = function(){return funcion(this);}
+    formulario.appendChild(unidades);
+    formulario.appendChild(codigo);
+    formulario.appendChild(boton);
+    return formulario;
+}
+
+function crearFila(elementos, tipo) {
+    var tr = document.createElement("tr");
+
+    for(var i = 0; i<elementos.length; i++){
+        var datos = document.createElement(tipo);
+        datos.innerHTML = elementos[i];
+        tr.appendChild(datos);
+    }
+
+    return tr;
+}
+
+function cargarCarrito(){
+    var XMLHttpRequestObject = new XMLHttpRequest();
+
+    XMLHttpRequestObject.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var contenido = document.getElementById("contenido");
+            var titulo = document.getElementById("titulo");
+            titulo.innerHTML = "Carrito";
+            try {
+                var productos_json = JSON.parse(this.responseText);
+                var tabla = crearTablaCarrito(productos_json);
+                contenido.innerHTML = "";
+                contenido.appendChild(tabla);
+
+                var confirmar = document.createElement("a");
+                confirmar.href = "#";
+                confirmar.onclick = function(){return procesarPedido();}
+                confirmar.innerHTML = "Confirmar pedido";
+                contenido.appendChild(confirmar);
+            } catch(e) {
+                var mensaje = document.createElement("p");
+                mensaje.innerHTML = "Carrito sin producto";
+                contenido.innerHTML = "";
+                contenido.appendChild(mensaje);
+            }
+        }
+    };
+    XMLHttpRequestObject.open("GET", "server_carrito.php", true);
+    XMLHttpRequestObject.send();
+    return false;
+}
+
+function crearTablaCarrito(productos) {
+    var tabla = document.createElement("tabla");
+    var cabecera = crearFila(["Código", "Nombre", "Descripción", "Unidades", "Eliminar"], "th");
+    tabla.appendChild(cabecera);
+    for(var i = 0; i<productos.length; i++){
+        formulario = crearFormulario("Eliminar", productos[i]["producto_id"], eliminarProductos);
+        fila = crearFila([productos[i]["producto_id"],
+                        productos[i]["nombre"],
+                        productos[i]["descripción"],
+                        productos[i]["unidades"]], "td");
+        celda = document.createElement("td");
+        celda.appendChild(formulario);
+        fila.appendChild(celda);
+        tabla.appendChild(fila);
+    }
+    return tabla;
+}
+
+function anadirProductos(formulario) {
+    var XMLHttpRequestObject = new XMLHttpRequest();
+
+    XMLHttpRequestObject.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            alert("Producto añadido con éxito");
+            cargarCarrito();
+        }
+    };
+    var params = "cod=" + formulario.elements["cod"].value + "&unidades=" + formulario.elements["unidades"].value;
+    XMLHttpRequestObject.open("POST", "server_productosAnadir.php", true);
+    XMLHttpRequestObject.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    XMLHttpRequestObject.send(params);
+    return false;
+}
+
+function procesarPedido() {
+    var XMLHttpRequestObject = new XMLHttpRequest();
+
+    XMLHttpRequestObject.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            alert("Entra");
+            var contenido = document.getElementById("contenido");
+            var titulo = document.getElementById("título");
+            contenido.innerHTML = "";
+            titulo.innerHTML = "Estado del pedido";
+            if(this.responseText == "TRUE") {
+                contenido.innerHTML = "Pedido realizado";
+            } else {
+                contenido.innerHTML = "Error al procesar el pedido";
+            }
+        }
+    };
+
+    XMLHttpRequestObject.open("GET", "server_procesarPedido.php", true);
+    XMLHttpRequestObject.send();
+    return false;
+}
